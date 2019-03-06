12c61635dc551cd747d353c55ed18b6b51e9f81e
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-06 10:43:46 +0530

    passing end condition

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,6 +1,9 @@
 public class Recursion {
 //void countDown(int number){
 static void countDown(int number){
+    if(number <=0){
+
+    }
     System.out.println(number);
     --number;
     countDown(number);
