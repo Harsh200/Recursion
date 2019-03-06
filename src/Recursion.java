0e93f70294a82ef52e37e7e999d09d52fbc2436a
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-06 11:03:24 +0530

    factorial program

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,25 +1,25 @@
-public class Recursion {
-//void countDown(int number){
-// static void countDown(int number){
-////     if(number <=0){
-////         return;
-////    }
-////    System.out.println(number);
-////    --number;
-////    countDown(number);
-    static  int sum(int number){
-        if(number ==1){
-            return 1;
-        }
-        return number + sum(--number);
-
-
-
-}
-
-    public static void main(String[] args) {
-    // Recursion recursion=new Recursion();
-        System.out.println( sum(4));
-
-    }
-}
+//public class Recursion {
+////void countDown(int number){
+//// static void countDown(int number){
+//////     if(number <=0){
+//////         return;
+//////    }
+//////    System.out.println(number);
+//////    --number;
+//////    countDown(number);
+//    static  int sum(int number){
+//        if(number ==1){
+//            return 1;
+//        }
+//        return number + sum(--number);
+//
+//
+//
+//}
+//
+//    public static void main(String[] args) {
+//    // Recursion recursion=new Recursion();
+//        System.out.println( sum(4));
+//
+//    }
+//}
