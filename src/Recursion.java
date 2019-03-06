d7ab0757e822c8bfdfd867736e980cfd5a18b219
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-06 10:45:38 +0530

    return but void

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,8 +1,8 @@
 public class Recursion {
 //void countDown(int number){
 static void countDown(int number){
-    if(number <=0){
-
+     if(number <=0){
+         return;
     }
     System.out.println(number);
     --number;
