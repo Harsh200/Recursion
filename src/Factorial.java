be3c0f0a984a13f62a6ec7f10a9cdf45bb6756fa
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-06 11:09:03 +0530

    End of factorial program

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,14 +1,26 @@
 public class Factorial {
 
         static int factorial(int n){
-            if (n == 1)
+//            if (n == 1)
+//                return 1;
+//
+//            else
+//                return(n * factorial(n-1));
+//        }
+            if(n==1) {
                 return 1;
-            else
-                return(n * factorial(n-1));
-        }
+            }
+            else{
+                return  (n* factorial(n-1));
+            }
+            }
 
         public static void main(String[] args) {
-            System.out.println("Factorial of 4 is: "+factorial(5));
+            System.out.println("Factorial of 6 is: "+factorial(6));
         }
+
+
+
+
     }
 
