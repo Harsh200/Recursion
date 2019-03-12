6118d1475023ea1f4d589781102a4dbcd75040db
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-12 14:19:09 +0530

    creating constructor

--- src/Stack.java
+++ src/Stack.java
@@ -3,7 +3,9 @@ public class Stack {
         private int data;
         Node next;
 
-
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next=next;
         }
     }
 }
