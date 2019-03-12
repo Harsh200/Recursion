252c309e9fe43c1566423c5a3721933d49714c4a
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-12 14:49:26 +0530

    creating construcor of node class

--- src/queue.java
+++ src/queue.java
@@ -1,5 +1,11 @@
 public class queue {
     private static class Node{
+private int data;
+private Node next;
 
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
     }
 }
