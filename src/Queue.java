ea2633b7c71790812aff08d376113de7803a6ece
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-12 15:01:25 +0530

    creating enqueue method

--- src/Queue.java
+++ src/Queue.java
@@ -30,6 +30,30 @@ private Node next;
 
     public static void main(String[] args) {
     Queue queue= new Queue();
-
+        System.out.println(queue);
+        for (int i = 0; i < 10; i++) {
+         queue.enqueue(i+1);
+        }
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+    }
+    @Override
+    public String toString(){
+        StringBuilder result=new StringBuilder();
+        result.append("[");
+        Node temp=this.front;
+        while (temp!=null){
+            result.append(temp.getData());
+            if(temp.getNext()!=null){
+                result.append(" --");
+            }
+            temp=temp.getNext();
+        }
+        result.append("]");
+        return result.toString();
+    }
+    public void enqueue(int data){
+        Node node=new Node(data,null);
     }
 }
