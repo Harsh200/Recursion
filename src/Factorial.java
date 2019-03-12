8a2283798bf698392a566874c5376126e86f7cb2
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-12 14:15:58 +0530

    this is a program for implementation  of stack using linkedlist

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,26 +1,26 @@
-public class Factorial {
-
-        static int factorial(int n){
-//            if (n == 1)
-//                return 1;
+//public class Factorial {
 //
-//            else
-//                return(n * factorial(n-1));
+//        static int factorial(int n){
+////            if (n == 1)
+////                return 1;
+////
+////            else
+////                return(n * factorial(n-1));
+////        }
+//            if(n==1) {
+//                return 1;
+//            }
+//            else{
+//                return  (n* factorial(n-1));
+//            }
+//            }
+//
+//        public static void main(String[] args) {
+//            System.out.println("Factorial of 6 is: "+factorial(6));
 //        }
-            if(n==1) {
-                return 1;
-            }
-            else{
-                return  (n* factorial(n-1));
-            }
-            }
-
-        public static void main(String[] args) {
-            System.out.println("Factorial of 6 is: "+factorial(6));
-        }
-
-
-
-
-    }
-
+//
+//
+//
+//
+//    }
+//
