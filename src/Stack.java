1a57120caa43a35be0db4066e421d8edcc9eae1b
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-12 14:34:33 +0530

    creating loop

--- src/Stack.java
+++ src/Stack.java
@@ -50,7 +50,9 @@ public class Stack {
     public static void main(String[] args) {
         Stack stack = new Stack();
         System.out.println(stack);
+        for (int i = 0; i < 5; i++) {
 
+        }
 
 
     }
